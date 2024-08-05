5d140d7fa2f99e66e952ceceffba06056774e720
Andrew Lobban <andrewglobban@gmail.com>
2024-08-05 10:33:43 -0500

    TDD Ticket routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@al_tickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@al_tickets/common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +15,9 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
