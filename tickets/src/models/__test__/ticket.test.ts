74898dd87446d0c7db86994d7913c24b4ee046b7
Andrew Lobban <andrewglobban@gmail.com>
2025-04-02 11:20:57 -0500

    Implement OCC and supportive tests

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,50 @@
+import { Ticket } from '../tickets';
+
+it('implements optoimistic concurrency control', async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make 2 separate changes to the ticket we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // save the first fetched ticket
+  await firstInstance!.save();
+
+  // save the second fetched ticket and expect an error
+  expect(async () => {
+    await secondInstance!.save();
+  }).rejects.toThrow();
+
+  // ******** ALTERNATIVE APPROACH ********
+  // try {
+  //   await secondInstance!.save();
+  // } catch (err) {
+  //   return;
+  // }
+  // throw new Error('Should not reach this point');
+});
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+});
