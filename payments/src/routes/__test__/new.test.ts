c88e720d6b091c2f3fcb8963a7fb7bf89b2a9e37
Andrew Lobban <andrewglobban@gmail.com>
2025-04-09 14:18:41 -0500

    Mock & test Stripe

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import { OrderStatus } from '@al_tickets/common';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 if the route does not exist', async () => {
   await request(app)
@@ -56,4 +59,35 @@ it('returns a 400 when purchasing a cancelled order', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: userId,
+    version: 0,
+    price: 10,
+    status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signup(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  // console.log(
+  //   'charge options',
+  //   (stripe.charges.create as jest.Mock).mock.calls[0][0]
+  // );
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.amount).toEqual(10 * 100);
+  expect(chargeOptions.currency).toEqual('usd');
 });
