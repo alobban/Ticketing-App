b3a7042b985ddfb2ac491a27c9db07a3096b7d41
Andrew Lobban <andrewglobban@gmail.com>
2025-04-09 14:05:32 -0500

    Remove charge variable

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -38,7 +38,7 @@ router.post(
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
-    const charge = await stripe.charges.create({
+    await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100,
       source: token,
