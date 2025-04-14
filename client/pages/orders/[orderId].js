d7e6974a25a96ecfcb0489dc0c51b748b7a69f81
Andrew Lobban <andrewglobban@gmail.com>
2025-04-14 15:16:52 -0500

    Display expiration countdown timer

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,9 +1,32 @@
-const ShowOrder = () => {
-  return (
-    <div>
-      <h1>Show Order</h1>
-      {/* Add your order details here */}
-    </div>
-  );
+import { useEffect, useState } from 'react';
+
+const ShowOrder = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    calculateTimeLeft();
+    const interval = setInterval(calculateTimeLeft, 1000);
+
+    return () => clearInterval(interval);
+  }, [order.expiresAt]);
+
+  if (timeLeft < 0) {
+    return <div>Order expired</div>;
+  }
+
+  return <div>Time left to pay: {timeLeft} seconds</div>;
 };
+
+ShowOrder.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
 export default ShowOrder;
