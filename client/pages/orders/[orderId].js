85e6151259c0d5d17e526309ee71fd9edcc3352c
Andrew Lobban <andrewglobban@gmail.com>
2025-04-14 15:50:49 -0500

    Install & configure Stripe Checkout payment modal

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-const ShowOrder = ({ order }) => {
+const STRIPE_KEY =
+  'pk_test_51RC3Nj4KXGSv3yJm3RCGeCox4snGTOq6vRV2uRU8Ebjf0dg7wPfZ1Nw5dFj4lsdzdZ7o7PpqjiicfAEIwljjRAWk00WAryUFPO';
+
+const ShowOrder = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -19,7 +23,18 @@ const ShowOrder = ({ order }) => {
     return <div>Order expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        key={order.id}
+        token={(token) => console.log(token)}
+        stripeKey={STRIPE_KEY}
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
 };
 
 ShowOrder.getInitialProps = async (context, client) => {
