039683e42a7660afb1dc0ae8f3cb28a29e072020
Andrew Lobban <andrewglobban@gmail.com>
2025-04-14 14:45:19 -0500

    Add Purchase functionality

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,14 +1,32 @@
-import { useEffect, useState } from 'react';
+import useRequest from '../../hooks/use-request';
 
-const ShowTicket = ({ ticketId, client }) => {
-  const [ticket, setTicket] = useState(null);
+const ShowTicket = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => console.log(order),
+  });
 
   return (
     <div>
-      <h1>Show Ticket</h1>
-      <p>This is the ticket detail page.</p>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">
+        Purchase
+      </button>
     </div>
   );
+};
+
+ShowTicket.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
 };
 
 export default ShowTicket;
