060a75c82f5564e09d1f818c21117383ce1b9339
Andrew Lobban <andrewglobban@gmail.com>
2025-04-14 14:57:01 -0500

    Route to Show Order page on Ticket Purchase button

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,9 @@
+const ShowOrder = () => {
+  return (
+    <div>
+      <h1>Show Order</h1>
+      {/* Add your order details here */}
+    </div>
+  );
+};
+export default ShowOrder;
--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const ShowTicket = ({ ticket }) => {
@@ -7,7 +8,8 @@ const ShowTicket = ({ ticket }) => {
     body: {
       ticketId: ticket.id,
     },
-    onSuccess: (order) => console.log(order),
+    onSuccess: (order) =>
+      Router.push('/orders/[orderId]', `/orders/${order.id}`),
   });
 
   return (
